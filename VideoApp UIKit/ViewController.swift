3779effc28d5e0f37459d31c8e6bdbd0b3b2c3ac
Alex Hwan <for.hwan@gmail.com>
2022-10-31 17:20:13 +0600

    Initial Commit

---
+++ VideoApp UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoApp UIKit
+//
+//  Created by Alex Hwan on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
